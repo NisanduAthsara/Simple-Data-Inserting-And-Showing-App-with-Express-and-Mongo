9cbad151686d9b10ce92961d03819ca9e146140e
NisanduAthsara <nisandunox@gmail.com>
2022-03-07 16:41:31 +0530

    Add 404 page and New User Page

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ app.set('view engine','ejs')
 
 //set public assets
 app.use('/css',express.static(path.join(__dirname,'assets/css')))
+app.use('/img',express.static(path.join(__dirname,'assets/img')))
 
 //use routes
 app.use(router)
+
+app.get('*',(req,res)=>{
+    res.render('error404',{title:'Error 404'})
+})
 
 app.listen(PORT,()=>{
     console.log(`http://localhost:${PORT}`);
--- server/router/router.js
+++ server/router/router.js
@@ -5,4 +5,8 @@ router.get('/',(req,res)=>{
     res.render('index',{title:'Dashboard'})
 })
 
+router.get('/add-new',(req,res)=>{
+    res.render('add_new',{title:'Add New User'})
+})
+
 module.exports = router
